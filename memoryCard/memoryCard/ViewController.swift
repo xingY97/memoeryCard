4394c0f5ad9bc29368ed058bb5f197adde2b22db
Xing_Ye <YOUR_PERSONAL_EMAIL@example.com>
2020-08-27 16:29:55 -0400

    collective view added

---
+++ memoryCard/memoryCard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  memoryCard
+//
+//  Created by XIN on 8/27/20.
+//  Copyright © 2020 XIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
